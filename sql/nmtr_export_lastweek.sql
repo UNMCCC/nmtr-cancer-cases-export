0c9a9ec21d6d302b28d177bc96eaf0d45062ac8d
Inigo San Gil <isangil@lternet.edu>
2020-04-17 13:55:00 -0600

    updates freq to weekly, drops CPTs

--- sql/nmtr_export_lastweek.sql
+++ sql/nmtr_export_lastweek.sql
@@ -1,11 +1,11 @@
 /*1.23.15 - KB modified since we are not closing ledger after CLS_ID = 80 (Nov2014); update the whole query to point to Charge table (confirmed w/JP)*/
 /*KB modified 8/29/12 to take out trx_amt and modified Aclose.CLS_ID to prior month
-KB modified 4-26-12 for 2.3 Upgrade (ICD9 fields had to be linked, etc.)
-ISG Jan-24-2018.  Changed to remove actual CPTs, which are not usable by NMTR.  Stop at distinct MRN-DOS-DX, reduces the list.4*/
+KB modified 4-26-12 for 2.3 Upgrade (ICD9 fields had to be linked, etc.)*/
+/*IS modified -- dropped CPT codes from list --date range now week (last week)*/
 
-declare @TodayMonthAgo as Date = dateadd(month,-1,GetDate())
-declare @StartOfMonth as Date = DateAdd( day, 1 - Day( @TodayMonthAgo ), @TodayMonthAgo )
-declare @EndOfMonth as Date = DateAdd( day, -1, DateAdd( month, 1, @StartOfMonth ) )
+declare @TodayWeekAgo as Date = dateadd(week,-1,GetDate())
+declare @StartOfWeek as Date =  dateadd(week, datediff(week, 0,  @TodayWeekAgo), 0);
+declare @EndOfWeek as Date = dateadd(week, datediff(week, 0, @TodayWeekAgo), 5);
 
 Set nocount on
 
@@ -21,16 +21,20 @@ SELECT distinct
  Admin.Pat_State AS State, 
  Admin.Pat_Postal AS Zip, 
  replace(patient.SS_Number,'-','') AS SSN,
- Convert(char(10),Patient.Birth_DtTm,112) AS DOB, 
+ Convert(char(10),Patient.Birth_DtTm,101) AS DOB, 
  Ident.IDC AS DOD, 
- Convert(char(10),Admin.Expired_DtTm,112) AS Expired_DtTm,
- Convert(char(10),CHG.Proc_DtTm,112) AS DOS,
+ Convert(char(10),Admin.Expired_DtTm,101) AS Expired_DtTm,
+ Convert(char(10),CHG.Proc_DtTm,101) AS DOS,
  dbo.fn_FormatName(Staff.Last_Name, Staff.First_Name, Staff.Mdl_Initial, 'NAMEFML', 'STAFF') AS Physician, 
  RTRIM (ISNULL(T1.Diag_code,'')) AS Dx1, 
  RTRIM (ISNULL(T2.Diag_code,'')) AS Dx2, 
  RTRIM (ISNULL(T3.Diag_code,'')) AS Dx3, 
- RTRIM (ISNULL(T4.Diag_code,'')) AS Dx4
-
+ RTRIM (ISNULL(T4.Diag_code,'')) AS Dx4, 
+ Admin.Pat_Home_Phone
+/* CPT.CPT_Code AS CPTCode, 
+ CPT.Description AS CPTDes,
+ CHG.Exported_Prof_DtTm,
+ CHG.Exported_Tech_DtTm*/
 FROM         
  Charge CHG
  INNER JOIN Patient ON CHG.Pat_ID1 = Patient.Pat_ID1 
@@ -44,9 +48,10 @@ FROM
  LEFT OUTER JOIN Topog T3 ON CHG.TPG_ID3 = T3.TPG_ID
  LEFT OUTER JOIN Topog T4 ON CHG.TPG_ID4 = T4.TPG_ID
  WHERE  
-CONVERT(CHAR(8),Exported_Prof_DtTm,112) between @StartOfMonth and @EndOfMonth /*using only prof export date*/
+CONVERT(CHAR(8),Exported_Prof_DtTm,112) between @StartOfWeek and @EndOfWeek /*using only prof export date*/
 and CPT.Billable in ('3','2') /*Prof and Tech only for */
 and Facility.Name like 'UNMMG%'
+  --and CPT.CPT_Code <> 'PREPY'
  
 ORDER BY 
  LastName, DOS
